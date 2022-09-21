3ecb63f377c8a58e639f7400a7b3a5bebd1643c2
unknown <prasad.xworkz@gmail.com>
2022-09-21 21:07:05 +0530

    array related java files

---
+++ User.java
@@ -0,0 +1,21 @@
+class User
+{
+
+static String cards[]= {"palatinum cerdit cards","millenia cerdit cards",
+                       "kotak urbane card","sbi cerdit card"," kvb cerdit/debit cards"};
+
+public static void main(String a[])
+{
+
+System.out.println("welcome to bank cards");
+System.out.println("no of cards is "+ cards.length);
+for(int i=0;i< cards.length;i++)
+
+{
+
+System.out.println(cards[i]);
+}
+
+ 
+}
+}
